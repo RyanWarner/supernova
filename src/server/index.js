be9cc1947cfb4f7f90967021f8643a6784457181
Ryan Warner <warner.ryan@gmail.com>
2019-07-11 00:01:34 -0400

    Lazy load server deps

--- src/server/index.js
+++ src/server/index.js
@@ -1,13 +1,4 @@
 const express = require('express')
-const cors = require('cors')
-const path = require('path')
-const chalk = require('chalk')
-const manifestHelpers = require('express-manifest-helpers').default
-const bodyParser = require('body-parser')
-const configureStore = require('../shared/store').configureStore
-const serverRender = require('./render').default
-const paths = require('../../config/paths')
-const cookieParser = require('cookie-parser')
 
 const isDev = process.env.NODE_ENV === 'development'
 
@@ -15,63 +6,77 @@ require('dotenv').config()
 
 const app = module.exports = express()
 
-if (isDev) {
-  app.use(paths.publicPath, express.static(paths.clientBuild))
-  app.use('/favicon.ico', (req, res) => {
-    res.send('')
+const initializeApp = () => {
+  const cors = require('cors')
+  const path = require('path')
+  const manifestHelpers = require('express-manifest-helpers').default
+  const bodyParser = require('body-parser')
+  const configureStore = require('../shared/store').configureStore
+  const serverRender = require('./render').default
+  const paths = require('../../config/paths')
+  const cookieParser = require('cookie-parser')
+
+  if (isDev) {
+    app.use(paths.publicPath, express.static(paths.clientBuild))
+    app.use('/favicon.ico', (req, res) => {
+      res.send('')
+    })
+  }
+
+  app.use(cors())
+
+  app.use(bodyParser.json())
+  app.use(cookieParser())
+
+  app.use((req, res, next) => {
+    const initialState = (req.cookies && req.cookies.__session) ? JSON.parse(req.cookies.__session) : {}
+    req.store = configureStore({
+      initialState
+    })
+    return next()
   })
-}
 
-app.use(cors())
+  const manifestBase = isDev ? paths.manifest : paths.manifestLocal
+  const manifestPath = `${manifestBase}/manifest.json`
 
-app.use(bodyParser.json())
-app.use(cookieParser())
+  app.use(manifestHelpers({ manifestPath }))
 
-app.use((req, res, next) => {
-  const initialState = (req.cookies && req.cookies.__session) ? JSON.parse(req.cookies.__session) : {}
-  req.store = configureStore({
-    initialState
-  })
-  return next()
-})
+  app.use(serverRender())
 
-const manifestBase = isDev ? paths.manifest : paths.manifestLocal
-const manifestPath = `${manifestBase}/manifest.json`
-
-app.use(manifestHelpers({ manifestPath }))
-
-app.use(serverRender())
-
-// eslint-disable-next-line no-unused-vars
-app.use((err, req, res, next) => {
-  return res.status(404).json({
-    status: 'error',
-    message: err.message,
-    stack:
-      // print a nicer stack trace by splitting line breaks and making them array items
-      process.env.NODE_ENV === 'development' &&
-      (err.stack || '')
-        .split('\n')
-        .map((line) => line.trim())
-        .map((line) => line.split(path.sep).join('/'))
-        .map((line) =>
-          line.replace(
-            process
-              .cwd()
-              .split(path.sep)
-              .join('/'),
-            '.'
+  // eslint-disable-next-line no-unused-vars
+  app.use((err, req, res, next) => {
+    return res.status(404).json({
+      status: 'error',
+      message: err.message,
+      stack:
+        // print a nicer stack trace by splitting line breaks and making them array items
+        process.env.NODE_ENV === 'development' &&
+        (err.stack || '')
+          .split('\n')
+          .map((line) => line.trim())
+          .map((line) => line.split(path.sep).join('/'))
+          .map((line) =>
+            line.replace(
+              process
+                .cwd()
+                .split(path.sep)
+                .join('/'),
+              '.'
+            )
           )
-        )
+    })
   })
-})
 
-if (isDev) {
-  app.listen(process.env.PORT || 8080, () => {
-    console.log(
-      `[${new Date().toISOString()}]`,
-      chalk.hex('#88DEF3')(`🔥 App is running:`),
-      chalk.bold.hex('#A8ED4F')(`http://localhost:${process.env.PORT || 8080}`)
-    )
-  })
+  if (isDev) {
+    const chalk = require('chalk')
+    app.listen(process.env.PORT || 8080, () => {
+      console.log(
+        `[${new Date().toISOString()}]`,
+        chalk.hex('#88DEF3')(`🔥 App is running:`),
+        chalk.bold.hex('#A8ED4F')(`http://localhost:${process.env.PORT || 8080}`)
+      )
+    })
+  }
 }
+
+initializeApp()
