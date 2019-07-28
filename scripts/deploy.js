e14ed50cf360b97a16f70aaa5dc16d496845fea2
Ryan Warner <warner.ryan@gmail.com>
2019-07-28 12:00:58 -0400

    Create deploy node script

---
+++ scripts/deploy.js
@@ -0,0 +1,38 @@
+const paths = require('../config/paths')
+const DEPLOY_ENV = process.argv[2]
+console.log('process.argv', process.argv)
+require('dotenv').config({ path: `${paths.dotenv}.${DEPLOY_ENV}` })
+const util = require('util')
+const exec = util.promisify(require('child_process').exec)
+const chalk = require('chalk')
+
+// const DEPLOY_ENV = process.env.DEPLOY_ENV
+const chalkColor = '#3D87C9'
+
+const firebaseUse = async () => {
+  const message = `Using Firebase project: ${DEPLOY_ENV}`
+  console.log(chalk.hex(chalkColor).bold(message))
+
+  const { stdout, stderr } = await exec(`./node_modules/.bin/firebase use ${DEPLOY_ENV}`)
+  if (stdout) console.log('stdout', stdout)
+  if (stderr) console.log('stderr', stderr)
+}
+
+const firebaseDeploy = async () => {
+  const message = `Deploying to Firebase`
+  console.log(chalk.hex(chalkColor).bold(message))
+
+  const { stdout, stderr } = await exec(`./node_modules/.bin/firebase deploy --only hosting,functions`)
+  if (stdout) console.log('stdout', stdout)
+  if (stderr) console.log('stderr', stderr)
+}
+
+const deploy = async () => {
+  const message = `Deploying to: ${DEPLOY_ENV}`
+  console.log(chalk.hex(chalkColor).bold(message))
+
+  await firebaseUse()
+  await firebaseDeploy()
+}
+
+deploy()
--- src/shared/components/AppUIKit/TextField/styles.js
+++ src/shared/components/AppUIKit/TextField/styles.js
@@ -1,4 +1,4 @@
-import styled, { css } from 'styled-components'
+import styled from 'styled-components'
 import { BasicText } from 'informed'
 
 import { Colors, Type, Inputs } from 'app/styles'
