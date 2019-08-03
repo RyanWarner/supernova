dfab777408a0fb6e42b3d999891726d2e1e502b7
Ryan Warner <warner.ryan@gmail.com>
2019-08-03 09:37:33 -0400

    Add withServerData HOC

---
+++ src/shared/components/withServerData.js/withServerData.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+
+export default (serverFetch) => (Page) => {
+  class HOC extends Component {
+    static serverFetch = (context) => {
+      if (!serverFetch) {
+        console.error(`
+          withServerData requires a serverFetch function.\n
+          The serverFetch function must return a Redux action.
+        `)
+        return
+      }
+
+      return serverFetch(context)
+    }
+
+    render () {
+      return <Page />
+    }
+  }
+
+  return HOC
+}
