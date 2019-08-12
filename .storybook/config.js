935cdca180b6d82e6e0ac6f96afef919fb767995
Ryan Warner <warner.ryan@gmail.com>
2019-08-12 19:10:26 -0400

    Basic storybook with readme working

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,10 +1,22 @@
-import { configure, addDecorator } from '@storybook/react'
-import { addReadme } from 'storybook-readme'
+import React from 'react'
+import { configure, addDecorator, addParameters } from '@storybook/react'
+import { addReadme, configureReadme } from 'storybook-readme'
 
 import StoryContainer from './StoryContainer/StoryContainer'
 
 addDecorator(addReadme)
 addDecorator(StoryContainer)
+addParameters({
+  options: { showPanel: false },
+  readme: {
+    codeTheme: 'atom-dark',
+
+  }
+})
+
+configureReadme({
+  StoryPreview: ({ children }) => <div style={{margin: '30px 0'}}>{children}</div>,
+})
 
 function loadStories() {
   const req = require.context('../src', true, /\.stories\.js$/)
--- src/shared/components/AppUIKit/TextArea/README.md
+++ src/shared/components/AppUIKit/TextArea/README.md
@@ -1,1 +1,29 @@
 # Text Area
+
+Text area docs
+
+<!-- STORY -->
+
+```js
+import { withInformed } from 'app/components'
+import { TextArea } from 'app/ui-kit'
+
+const fieldConfig = {
+  message: {
+    label: 'Message',
+    field: 'message',
+    placeholder: 'Type your message',
+    validate: (value) => {
+      if (!value || value.length < 1) return 'Required'
+      return null
+    }
+  }
+}
+
+@withInformed
+class FormWrap extends React.Component {
+  render = () => {
+    return <TextArea {...fieldConfig.message} />
+  }
+}
+```
