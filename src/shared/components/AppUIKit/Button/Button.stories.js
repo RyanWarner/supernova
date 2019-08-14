7b70a9b0c9b4d9fd5146e2c417a3aaf845cab295
Ryan Warner <warner.ryan@gmail.com>
2019-08-14 17:12:52 -0400

    Capitalize first letter of button story

---
+++ src/shared/components/AppUIKit/Button/Button.stories.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import { withDocs } from 'storybook-readme'
+import { withKnobs, text, boolean } from '@storybook/addon-knobs'
+
+import Button from './Button'
+import Readme from './README.md'
+import { dirNames, titles } from '@/.storybook/names'
+
+storiesOf(`${titles.components}|${dirNames.uiKit}/Button`, module)
+  .addDecorator(withDocs(Readme))
+  .addDecorator(withKnobs)
+  .add('default', () => {
+    return <Button
+      loading={boolean('Loading', false)}
+      disabled={boolean('Disabled', false)}
+    >
+      { text('Text', 'Button') }
+    </Button>
+  })
