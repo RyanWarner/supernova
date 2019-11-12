94538e2247e680f7a09068d802b345f10dbde078
Roman Sorin <roman@romansorin.com>
2019-11-11 19:14:45 -0500

    Fn/class

--- config/plop/component/component.generator.js
+++ config/plop/component/component.generator.js
@@ -13,34 +13,84 @@ module.exports = {
       name: 'styledComponents',
       message: 'Do you want the component to be styled with Styled Components?',
       default: true
+    },
+    {
+      type: 'list',
+      name: 'componentType',
+      message: 'Select component type',
+      choices: ['class', 'function']
     }
   ],
-  actions: (answers) => {
+  actions: answers => {
     const actions = []
 
-    if (!answers.styledComponents) {
-      actions.push({
+    const componentTemplates = {
+      defaultClass: {
         type: 'add',
-        path: `${paths.components}/{{properCase componentName}}/{{properCase componentName}}.js`,
-        templateFile: './config/plop/component/component.js.plop'
-      })
+        path: `${
+          paths.components
+        }/{{properCase componentName}}/{{properCase componentName}}.js`,
+        templateFile: './config/plop/component/class/component.js.plop'
+      },
+      defaultFunction: {
+        type: 'add',
+        path: `${
+          paths.components
+        }/{{properCase componentName}}/{{properCase componentName}}.js`,
+        templateFile: './config/plop/component/function/component.js.plop'
+      },
+      styledClass: [
+        {
+          type: 'add',
+          path: `${
+            paths.components
+          }/{{properCase componentName}}/{{properCase componentName}}.js`,
+          templateFile: './config/plop/component/class/component.styled.js.plop'
+        },
+        {
+          type: 'add',
+          path: `${paths.components}/{{properCase componentName}}/styles.js`,
+          templateFile: './config/plop/component/styles.js.plop'
+        },
+        {
+          type: 'addComponentToIndex'
+        }
+      ],
+      styledFunction: [
+        {
+          type: 'add',
+          path: `${
+            paths.components
+          }/{{properCase componentName}}/{{properCase componentName}}.js`,
+          templateFile:
+            './config/plop/component/function/component.styled.js.plop'
+        },
+        {
+          type: 'add',
+          path: `${paths.components}/{{properCase componentName}}/styles.js`,
+          templateFile: './config/plop/component/styles.js.plop'
+        },
+        {
+          type: 'addComponentToIndex'
+        }
+      ]
+    }
+
+    if (answers.componentType === 'class') {
+      answers.styledComponents
+        ? actions.push(...componentTemplates.styledClass)
+        : actions.push(componentTemplates.defaultClass)
     } else {
-      actions.push({
-        type: 'add',
-        path: `${paths.components}/{{properCase componentName}}/{{properCase componentName}}.js`,
-        templateFile: './config/plop/component/component.styled.js.plop'
-      }, {
-        type: 'add',
-        path: `${paths.components}/{{properCase componentName}}/styles.js`,
-        templateFile: './config/plop/component/styles.js.plop'
-      }, {
-        type: 'addComponentToIndex'
-      })
+      answers.styledComponents
+        ? actions.push(...componentTemplates.styledFunction)
+        : actions.push(componentTemplates.defaultFunction)
     }
 
     actions.push({
       type: 'add',
-      path: `${paths.components}/{{properCase componentName}}/{{properCase componentName}}.stories.mdx`,
+      path: `${
+        paths.components
+      }/{{properCase componentName}}/{{properCase componentName}}.stories.mdx`,
       templateFile: './config/plop/component/component.story.mdx.plop'
     })
 
