a9f97536ddf3ea55bf23facd58e14cd338d403a7
Ryan Warner <warner.ryan@gmail.com>
2019-11-29 08:51:41 -0500

    Update nav

--- src/shared/data/index.js
+++ src/shared/data/index.js
@@ -1,1 +1,2 @@
 export formFields from './formFields'
+export navItems from './navItems'
---
+++ src/shared/data/navItems.js
@@ -0,0 +1,27 @@
+export default {
+  0: {
+    display: 'Docs',
+    type: 'link',
+    url: '/docs/getting-started/installation'
+  },
+  1: {
+    display: 'GitHub',
+    type: 'a',
+    url: 'https://github.com/yawnch/nova'
+  },
+  3: {
+    display: 'Boilerplates',
+    type: 'a',
+    url: 'https://yawnch.com/boilerplates'
+  },
+  4: {
+    display: 'Courses',
+    type: 'a',
+    url: 'https://yawnch.com/courses'
+  },
+  5: {
+    display: 'Community',
+    type: 'a',
+    url: 'https://yawnch.com/community'
+  }
+}
